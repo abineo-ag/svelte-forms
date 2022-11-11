39dcafcd929936fcc8644e08c43e39d0397f2c09
Christoph Jean-Luc Schneider <kopilot.schneider@protonmail.com>
2022-11-11 17:15:12 +0100

    add one more test for custom class names

--- tests/fieldState.test.ts
+++ tests/fieldState.test.ts
@@ -11,17 +11,39 @@ describe('fieldState', () => {
 	});
 
 	it('adds and removes default class names', () => {
+		const valid = 'valid';
+		const invalid = 'invalid';
+		const dirty = 'dirty';
 		const { destroy } = fieldState(node, { field });
 
-		expect(node.classList.contains('valid')).toEqual(true);
-		expect(node.classList.contains('invalid')).toEqual(false);
-		expect(node.classList.contains('dirty')).toEqual(false);
+		expect(node.classList.contains(valid)).toEqual(true);
+		expect(node.classList.contains(invalid)).toEqual(false);
+		expect(node.classList.contains(dirty)).toEqual(false);
 
 		field.set('x');
 
-		expect(node.classList.contains('valid')).toEqual(false);
-		expect(node.classList.contains('invalid')).toEqual(true);
-		expect(node.classList.contains('dirty')).toEqual(true);
+		expect(node.classList.contains(valid)).toEqual(false);
+		expect(node.classList.contains(invalid)).toEqual(true);
+		expect(node.classList.contains(dirty)).toEqual(true);
+
+		destroy();
+	});
+
+	it('adds and removes custom class names', () => {
+		const valid = 'alice';
+		const invalid = 'bob';
+		const dirty = 'carl';
+		const { destroy } = fieldState(node, { field, valid, invalid, dirty });
+
+		expect(node.classList.contains(valid)).toEqual(true);
+		expect(node.classList.contains(invalid)).toEqual(false);
+		expect(node.classList.contains(dirty)).toEqual(false);
+
+		field.set('x');
+
+		expect(node.classList.contains(valid)).toEqual(false);
+		expect(node.classList.contains(invalid)).toEqual(true);
+		expect(node.classList.contains(dirty)).toEqual(true);
 
 		destroy();
 	});
